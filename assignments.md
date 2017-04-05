355776d4d4109f9f8efb8bd72a92414745f85c68
Thomas Jones <thomasrjones211@gmail.com>
2017-04-05 00:46:14 -0600

    v2 Initial Submission

--- assignments.md
+++ assignments.md
@@ -1,82 +1,28 @@
 var colorMeta = {
-comboName:'Selesnya', comboCode:'colors="green,white"'
-comboName:'Orzhov', comboCode:'colors="black,white"'
-comboName:'Boros', comboCode:'colors="red,white"'
-comboName:'Azorius', comboCode:'colors=blue,white'
-comboName:'Dimir', comboCode:'colors="black,blue"'
-comboName:'Rakdos', comboCode:'colors="black,red"'
-comboName:'Golgari', comboCode:'colors="black,green"'
-comboName:'Izzet', comboCode:'colors="blue,red"'
-comboName:'Simic', comboCode:'colors="blue,green"'
-comboName:'Gruul', comboCode:'colors="green,red"'
-comboName:'Naya', comboCode:'colors="green,red,white"'
-comboName:'Esper', comboCode:'colors="black,blue,white"'
-comboName:'Grixis', comboCode:'colors="black,blue,red"'
-comboName:'Jund', comboCode:'colors="black,green,red"'
-comboName:'Bant', comboCode:'colors="blue,green&white"'
-comboName:'Abzan', comboCode:'colors="black,green,white"'
-comboName:'Temur', comboCode:'colors="blue,green,red"'
-comboName:'Jeskai', comboCode:'colors="blue,red,white"'
-comboName:'Mardu', comboCode:'colors="black,red,white"'
-comboName:'Sultai', comboCode:'colors="black,blue,green"'
-comboName:'Glint', comboCode:'colors="black,blue,green,red"'
-comboName:'Dune', comboCode:'colors="black,green,red,white"'
-comboName:'Ink', comboCode:'colors="blue,green,red,white"'
-comboName:'Witch', comboCode:'colors="black,blue,green,white"'
-comboName:'Yore', comboCode:'colors="black,blue,red,white"'
-comboName:'fiveColor', comboCode:'colors="black,blue,green,red,white"'
+["name":"Selesnya", "colors":"green,white"]
+["name":"Orzhov", "colors":"black,white"]
+["name":"Boros", "colors":"red,white"]
+["name":"Azorius", "colors":"blue,white"]
+["name":"Dimir", "colors":"black,blue"]
+["name":"Rakdos", "colors":"black,red"]
+["name":"Golgari", "colors":"black,green"]
+["name":"Izzet", "colors":"blue,red"]
+["name":"Simic", "colors":"blue,green"]
+["name":"Gruul", "colors":"green,red"]
+["name":"Naya", "colors":"green,red,white"]
+["name":"Esper", "colors":"black,blue,white"]
+["name":"Grixis", "colors":"black,blue,red"]
+["name":"Jund", "colors":"black,green,red"]
+["name":"Bant", "colors":"blue,green,white"]
+["name":"Abzan", "colors":"black,green,white"]
+["name":"Temur", "colors":"blue,green,red"]
+["name":"Jeskai", "colors":"blue,red,white"]
+["name":"Mardu", "colors":"black,red,white"]
+["name":"Sultai", "colors":"black,blue,green"]
+["name":"Glint", "colors":"black,blue,green,red"]
+["name":"Dune", "colors":"black,green,red,white"]
+["name":"Ink", "colors":"blue,green,red,white"]
+["name":"Witch", "colors":"black,blue,green,white"]
+["name":"Yore", "colors":"black,blue,red,white"]
+["name":"Domain", "colors":"black,blue,green,red,white"]
 }
-
-
-
-
-
-
-
-<!-- Expansions:
-
-Alliances = ALL
-Antiquities = ATQ
-Apocalypse = APC
-Arabian Nights = ARN
-Battle Royale Box Set =
-Beatdown Box Set =
-Chronicles =
-Exodus =
-Fallen Empires =
-Fifth Edition =
-Fourth Edition =
-Homelands =
-Ice Age =
-Invasion =
-Judgment =
-Legends =
-Legions =
-Limited Edition Alpha =
-Limited Edition Beta =
-Mercadian Masques =
-Nemesis =
-Odyssey =
-Onslaught =
-Planeshift =
-Portal =
-Portal Second Age =
-Portal Three Kingdoms =
-Prophecy =
-Revised Edition =
-Scourge =
-Sixth (Classic) Edition =
-Seventh Edition =
-Starter 1999 =
-Starter 2000 =
-Stronghold =
-Tempest =
-The Dark =
-Torment =
-Unglued =
-Unlimited Edition =
-Urza's Destiny =
-Urza's Legacy =
-Urza's Saga =
-Visions =
-Weatherlight = -->
---
+++ main1.js
@@ -0,0 +1,142 @@
+$("document").ready(function() {
+  $("button").click(function() {
+  })
+});
+
+
+$("#black").click(function() {
+  $(this).toggleClass("glassBlack").toggleClass("active")
+  // console.log("Black was clicked")
+})
+
+$("#blue").click(function() {
+  $(this).toggleClass("glassBlue").toggleClass("active")
+  // console.log("Blue was clicked")
+})
+
+$("#green").click(function() {
+  $(this).toggleClass("glassGreen").toggleClass("active")
+  // console.log("Green was clicked")
+})
+
+$("#red").click(function() {
+  $(this).toggleClass("glassRed").toggleClass("active")
+  // console.log("Red was clicked")
+})
+
+$("#white").click(function() {
+  $(this).toggleClass("glassWhite").toggleClass("active")
+  // console.log("White was clicked")
+})
+
+function getActive() {
+  var activeColors = "";
+  var activeDivs = $(".active");
+  var colorSearch = "&colors=";
+  var colorStr = "";
+  if(activeDivs == 0) {
+    return "something"
+  } else {
+  //console.log(activeDivs)
+    for (var i = 0; i < activeDivs.length; i++) {
+        colorStr += (activeDivs[i].id) +"|"
+     }
+     activeColors = colorSearch + (colorStr.slice(0,-1))
+    // console.log(activeColors)
+     return activeColors
+   }
+}
+
+function getType() {
+  var optType = $("#cardType").val();
+  var typeSearch = "&type=" + optType;
+  //console.log(optType)
+  if($("#cardType").val() === "") {
+    return ""
+  } else {
+  //console.log(typeSearch)
+  return typeSearch;
+  }
+}
+
+function getRarity() {
+  var optRarity = $("#rarity").val();
+  var raritySearch = "&rarity=" + optRarity;
+  if ($("#rarity").val() === "") {
+    return ""
+  } else {
+    //console.log(raritySearch)
+    return raritySearch;
+    }
+  }
+
+function getEternal() {
+  var optEternal = $("#setsEternal").val();
+  var eternalSearch = "&setName=" + optEternal;
+  if($("#setsEternal").val() === "") {
+    return ""
+  } else {
+  //console.log(eternalSearch)
+  return eternalSearch;
+  }
+}
+
+function getModern() {
+  var optModern = $("#setsModern").val();
+  var modernSearch = "&setName=" + optModern;
+  if($("#setsModern").val() === "") {
+    return ""
+  } else {
+  //console.log(modernSearch)
+  return modernSearch;
+  }
+}
+
+function getName() {
+  var addName = $("#nameSearch").val();
+  var textSearch = "&name=" + addName;
+  if($("#nameSearch").val() === "") {
+    return ""
+  } else {
+  //console.log(textSearch)
+  return textSearch;
+  }
+}
+
+$("#submit").click(function(event) {
+  event.preventDefault()
+
+  $(".pic-container").empty();
+  //var addTheme = $("#colorTheme").val();
+  var colorSelect = getActive();
+  var typeSelect = getType();
+  var raritySelect = getRarity();
+  var eternalSelect = getEternal();
+  var modernSelect = getModern();
+  var nameSelect = getName();
+
+  var baseUrl = "https://api.magicthegathering.io/v1/cards?"
+  var getUrl = baseUrl + colorSelect + typeSelect + raritySelect + eternalSelect + modernSelect + nameSelect;
+  console.log(getUrl)
+  $.get(getUrl).then(function(data) {
+    //console.log(data)
+    var cards = data.cards;
+    //array of cards
+    for (var i = 0; i < cards.length; i++) {
+      updatePage(cards[i]);
+    }
+  })
+  //console.log(addColors)
+  //console.log(addTheme)
+  // console.log(colorSelect)
+  // console.log(typeSelect)
+  // console.log(raritySelect)
+  // console.log(eternalSelect)
+  // console.log(modernSelect)
+  // console.log(nameSelect)
+})
+
+
+function updatePage(card) {
+    $(".pic-container").append('<img src="'+ card.imageUrl + '">')
+};
